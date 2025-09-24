727bcd4539224269fe5a8a4d8ca367e84aabe4a1
Esther White <esteecodes@gmail.com>
2025-09-24 20:40:40 +0300

    starting basic layout and light theme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/layout/header/header';
+import { Footer } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
